99e7a32a71b1ca93ac1e40a9d31f13069299f697
Ilia Bogachev <ilia_bogachev@epam.com>
2019-03-20 17:40:11 +0300

    add post method with validation filed for ProjectTasks

--- src/main/java/io/agileintelligence/projectboard/service/ProjectTaskService.java
+++ src/main/java/io/agileintelligence/projectboard/service/ProjectTaskService.java
@@ -1,5 +1,6 @@
 package io.agileintelligence.projectboard.service;
 
+import io.agileintelligence.projectboard.domain.ProjectTask;
 import io.agileintelligence.projectboard.repository.ProjectTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,14 @@ public class ProjectTaskService {
 
     @Autowired
     private ProjectTaskRepository taskRepository;
+
+    public ProjectTask saveOrUpdateProjectTask(ProjectTask projectTask) {
+
+        if (projectTask.getStatus() == null || projectTask.getStatus() == "") {
+            projectTask.setStatus("TO_DO");
+        }
+
+        return taskRepository.save(projectTask);
+
+    }
 }
